c552ae8413ad1c503d48c816523698d284b6a9cd
joy <desarrolloappsaztek@gmail.com>
2023-06-02 16:55:10 -0600

    Commit de respaldo

--- main.py
+++ main.py
@@ -4,15 +4,16 @@ from functions.config import getConfig
 
 def main():
     config = getConfig()
+    #config['sdf']
     lectorConnection = serialConnection.serialConnection(config['serial'], config['baudrate'])
     print(config['serial'], config['baudrate'])
     while(True):
         try:
             print("Entra")
             data = lectorConnection.retriveData()
-            print('data: ', str(data))
+            print('data: ', data)
             data = str(data).split('/')[9].split('\\')[0]
-            print('ticket: ' + data)
+            print('ticket: ', data)
             validateTicket(query_api.get_ticket_info(data).json())
         except IndexError:
             print("Error en la lectura del codigo qr. Por favor acerque de nuevo el codigo")        
